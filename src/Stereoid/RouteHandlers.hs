1bc515b93cf8bc65c3466d2d8eedbe0cb8bb2549
Mike Better <mbetter@gmail.com>
2012-02-21 05:19:22 +0000

    App newtype and HasAcidState typeclass instances

--- src/Stereoid/RouteHandlers.hs
+++ src/Stereoid/RouteHandlers.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE MultiParamTypeClasses, GeneralizedNewtypeDeriving, FlexibleInstances #-}
 module Stereoid.RouteHandlers where
 
+import Control.Applicative (Applicative, Alternative, (<$>))
+import Control.Monad.Trans.Control (MonadBaseControl)
+import Control.Monad (MonadPlus, mplus)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Reader (MonadReader, ReaderT(..), ask)
+import Happstack.Server ( HasRqData, WebMonad, FilterMonad, ServerMonad, Happstack, mapServerPartT)
 import Data.Ord (comparing)
 import Data.List (sortBy)
+import Data.Maybe (fromMaybe)
 import Control.Monad (msum, liftM)
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.UTF8 as B
@@ -9,12 +17,14 @@ import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as E
 import Data.Acid
+import Data.Acid.Custom
 import Data.Aeson
+import System.FilePath ((</>))
 import LastFM.Request
 import System.Process
 import System.Exit  ( ExitCode(..) )
 import Control.Applicative (optional)
-import Control.Monad.Trans     (MonadIO, liftIO)
+import Control.Monad.Trans     (MonadIO(..), liftIO)
 import Happstack.Server        (port      , Response     , ServerPartT, ok           , toResponse
                                ,simpleHTTP, nullConf     , seeOther   , dir          , notFound
                                ,seeOther  , asContentType, serveFile  , ToMessage(..), look      
@@ -27,9 +37,9 @@ import Happstack.Server.Modified (filePathSendAllowRange)
 import Happstack.Server.Internal.Types (Request(..),Method(..))
 import Happstack.Server.Monads (askRq)
 import Control.Concurrent (forkIO)
-import Web.Routes.Boomerang    
+import Web.Routes.Boomerang hiding ((</>))
 import Web.Routes              ( PathInfo(..), RouteT    , showURL , runRouteT
-                               , Site(..)    , setDefault, mkSitePI           )
+                               , Site(..)    , setDefault, mkSitePI, liftRouteT           )
 import Stereoid.Jobs
 import Stereoid.Auth
 import Stereoid.Routing
@@ -42,10 +52,11 @@ import qualified Stereoid.Types.Internal as I
 decodePolicy :: BodyPolicy
 decodePolicy = (defaultBodyPolicy "/tmp/" 0 1000 1000)
 
-site :: AcidState StereoidDb -> AcidState UserMap -> AcidState SessionMap -> String -> String -> Site Sitemap (ServerPartT IO Response)
+
+site :: AcidState StereoidDb -> AcidState UserMap -> AcidState SessionMap -> String -> String -> Site Sitemap (App Response)
 site sdb users sessions resourcedir statedir = setDefault Home $ boomerangSite (runRouteT (route sdb users sessions resourcedir statedir)) sitemap
 
-route :: AcidState StereoidDb -> AcidState UserMap -> AcidState SessionMap -> String -> String -> Sitemap -> RouteT Sitemap (ServerPartT IO) Response
+route :: AcidState StereoidDb -> AcidState UserMap -> AcidState SessionMap -> String -> String -> Sitemap -> RouteT Sitemap App Response
 route sdb users sessions resourcedir statedir url =
      do decodeBody decodePolicy
         rq <- askRq
@@ -68,36 +79,72 @@ route sdb users sessions resourcedir statedir url =
                     (JobInfo jobId)         -> chk $ jobData sdb jobId
                     (Catalogs)              -> chk $ catalogsAll sdb
                     (CatalogInfo catalogId) -> chk $ catalogData sdb catalogId
+             
              PUT -> case url of
-                    (Sessions)              -> chk $ ok (toResponse "Session extended.")
-                    (CatalogInfo catalogId) -> chk $ startJob sdb
+                   (Sessions)              -> chk $ ok (toResponse "Session extended.")
+                   (CatalogInfo catalogId) -> chk $ startJob sdb
+             
              POST -> case url of
                     (Sessions)              -> authorize users sessions
                     (AlbumArt albumId)      -> chk $ getArtFromUrl sdb albumId
                     (Users)                 -> addUser users
                     (Catalogs)              -> chk $ catalogNew sdb
+                    
 
-catalogNew :: AcidState StereoidDb -> RouteT Sitemap (ServerPartT IO) Response
+data Acid = Acid { acidStereoidDb    :: AcidState StereoidDb
+                 , acidUserMap      :: AcidState UserMap
+                 , acidSessionMap   :: AcidState SessionMap
+                 }
+
+withAcid :: Maybe FilePath -> (Acid -> IO a) -> IO a
+withAcid mBasePath action =
+    let basePath = fromMaybe "_state" mBasePath
+    in withLocalState (Just $ basePath </> "users") (UserMap emptyMap) $ \u ->
+       withLocalState (Just $ basePath </> "sessions") (SessionMap emptyMap) $ \s ->
+       withLocalState (Just $ basePath </> "stereoiddb") (sdbEmpty) $ \t ->
+           action (Acid t u s )
+
+newtype App a = App { unApp :: ServerPartT (ReaderT Acid IO) a }
+    deriving ( Functor, Alternative, Applicative, Monad, MonadPlus, MonadIO
+               , HasRqData, ServerMonad, WebMonad Response, FilterMonad Response
+               , Happstack, MonadReader Acid)
+
+instance (HasAcidState m a) => HasAcidState (RouteT url m) a where
+    getAcidState = liftRouteT getAcidState
+
+instance HasAcidState App StereoidDb where
+    getAcidState = acidStereoidDb    <$> ask
+
+instance HasAcidState App UserMap where
+    getAcidState = acidUserMap      <$> ask
+
+instance HasAcidState App SessionMap where
+    getAcidState = acidSessionMap   <$> ask
+
+runApp :: Acid -> App a -> ServerPartT IO a
+runApp acid (App sp) = mapServerPartT (flip runReaderT acid) sp
+
+catalogNew :: AcidState StereoidDb -> RouteT Sitemap App Response
 catalogNew sdb = undefined
 
-catalogsAll :: AcidState StereoidDb -> RouteT Sitemap (ServerPartT IO) Response
+catalogsAll :: AcidState StereoidDb -> RouteT Sitemap App Response
 catalogsAll sdb = undefined
 
-catalogData :: AcidState StereoidDb -> CatalogId -> RouteT Sitemap (ServerPartT IO) Response
+catalogData :: AcidState StereoidDb -> CatalogId -> RouteT Sitemap App Response
 catalogData sdb (CatalogId id) = undefined
 
-startJob :: AcidState StereoidDb -> RouteT Sitemap (ServerPartT IO) Response
+startJob :: AcidState StereoidDb -> RouteT Sitemap App Response
 startJob sdb = do
     id <- getFreeJobId sdb  
     liftIO $ forkIO $ addToStereoidDb id "/mnt/emusic" sdb
     ok $ toResponse "Job started"
 
-jobsAll :: AcidState StereoidDb -> RouteT Sitemap (ServerPartT IO) Response
+jobsAll :: AcidState StereoidDb -> RouteT Sitemap App Response
 jobsAll sdb = do
     jobs <- getJobs sdb
     ok $ toResponse $ toJSON jobs
 
-jobData :: AcidState StereoidDb -> JobId -> RouteT Sitemap (ServerPartT IO) Response
+jobData :: AcidState StereoidDb -> JobId -> RouteT Sitemap App Response
 jobData sdb (JobId id) = do
     job <- getJob sdb id
     case job of
@@ -105,8 +152,8 @@ jobData sdb (JobId id) = do
         Just x  -> ok $ toResponse $ toJSON x
 
 checkToken :: AcidState SessionMap -> Integer -> 
-              RouteT Sitemap (ServerPartT IO) Response -> 
-              RouteT Sitemap (ServerPartT IO) Response
+              RouteT Sitemap App Response -> 
+              RouteT Sitemap App Response
 checkToken sessions min f = do
     token <- msum [lookCookieValue "token", look "token"]
     user <- checkExtendSession sessions 15 token   
@@ -114,7 +161,7 @@ checkToken sessions min f = do
         Nothing -> unauthorized $ toResponse $ "Invalid token"
         Just _  -> f
 
-addUser :: AcidState UserMap -> RouteT Sitemap (ServerPartT IO) Response
+addUser :: AcidState UserMap -> RouteT Sitemap App Response
 addUser acid = do
     user <- look "username"
     pass <- look "password"
@@ -123,7 +170,7 @@ addUser acid = do
         Right _ -> ok (toResponse $ "User " ++ user ++ " created.")
         Left m  -> forbidden (toResponse $ show m)
 
-authorize :: AcidState UserMap -> AcidState SessionMap -> RouteT Sitemap (ServerPartT IO) Response
+authorize :: AcidState UserMap -> AcidState SessionMap -> RouteT Sitemap App Response
 authorize users sessions = do
     username <- look "username"
     lt <- getDataFn $ look "logintoken"
@@ -149,7 +196,7 @@ authorize users sessions = do
                    sess <- newSession sessions 60 username
                    ok $ toResponse $ toJSON Remember { rSessionToken = sess, rRememberToken = newtoken }
             
-songAddUrl :: I.Song -> RouteT Sitemap (ServerPartT IO) Song
+songAddUrl :: I.Song -> RouteT Sitemap App Song
 songAddUrl I.Song { I.songID  = id
                   , I.songName = name
                   , I.songTrack = track
@@ -170,7 +217,7 @@ songAddUrl I.Song { I.songID  = id
                                      , songDuration = time
                                      } 
 
-albumAddUrl :: I.Album -> RouteT Sitemap (ServerPartT IO) Album
+albumAddUrl :: I.Album -> RouteT Sitemap App Album
 albumAddUrl I.Album { I.albumID = id
                     , I.albumTitle = title
                     , I.albumYear = year
@@ -191,13 +238,13 @@ albumAddUrl I.Album { I.albumID = id
                                         , albumM3UUrl = murl
                                         }
 
-albumSongs :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap (ServerPartT IO) Response
+albumSongs :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap App Response
 albumSongs sdb (AlbumId id) = do
     sgs <- getSongsByAlbumId sdb id
     songs <- mapM songAddUrl sgs 
     ok $ toResponse $ toJSON $ sortBy (comparing songTrack) songs 
 
-albumSongsM3U :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap (ServerPartT IO) Response
+albumSongsM3U :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap App Response
 albumSongsM3U sdb (AlbumId id) = do
     sgs <- getSongsByAlbumId sdb id
     songs <- mapM songAddUrl sgs 
@@ -205,7 +252,7 @@ albumSongsM3U sdb (AlbumId id) = do
         [] -> notFound $ toResponse "What you are looking for has not been found."
         xs -> ok $ toResponse $ createM3u xs
 
-albumData :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap (ServerPartT IO) Response
+albumData :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap App Response
 albumData sdb (AlbumId albumid) = do
     album <- getAlbum sdb albumid
     case album of
@@ -213,7 +260,7 @@ albumData sdb (AlbumId albumid) = do
         Just al -> do alb <- albumAddUrl al
                       ok $ toResponse $ toJSON alb
 
-getArtFromUrl :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap (ServerPartT IO) Response
+getArtFromUrl :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap App Response
 getArtFromUrl sdb (AlbumId artid) = do
     qs <- getDataFn $ lookRead "url"
     al <- getAlbum sdb artid
@@ -235,7 +282,7 @@ getArtFromUrl sdb (AlbumId artid) = do
         _            -> notFound $ toResponse "What you are looking for has not been found."
 
 {-
-getThumbFromUrl :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap (ServerPartT IO) Response
+getThumbFromUrl :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap App Response
 getThumbFromUrl sdb (AlbumId artid) = do
     url <- look "url"
     image <- liftIO $ downloadFileWithMime url
@@ -246,14 +293,14 @@ getThumbFromUrl sdb (AlbumId artid) = do
                                       ok $ toResponse "Thumb added."
                                       where afn x = ("thumb/" ++ (show x))
 -}
-serveArt :: AcidState StereoidDb -> String -> String -> AlbumId -> RouteT Sitemap (ServerPartT IO) Response
+serveArt :: AcidState StereoidDb -> String -> String -> AlbumId -> RouteT Sitemap App Response
 serveArt sdb rd sd (AlbumId artid) = do
     dr <- getArt sdb artid
     case dr of
         Just (mime,art) -> serveFileUsing filePathSendAllowRange (asContentType $ B.toString mime) $ (sd ++ art)
         Nothing         -> serveFile (asContentType "image/png") (rd ++ "media_album.png")
 
-serveGenThumb :: AcidState StereoidDb -> String -> String -> AlbumId -> RouteT Sitemap (ServerPartT IO) Response
+serveGenThumb :: AcidState StereoidDb -> String -> String -> AlbumId -> RouteT Sitemap App Response
 serveGenThumb sdb rd sd (AlbumId artid) = do
     dr <- getThumb sdb artid
     case dr of
@@ -271,40 +318,41 @@ serveGenThumb sdb rd sd (AlbumId artid) = do
                             serveFileUsing filePathSendAllowRange (asContentType $ B.toString amime) $ (sd ++ tfn)
                         ExitFailure _ -> serveFile (asContentType "image/png") (rd ++ "media_album.png")
         
-serveThumb :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap (ServerPartT IO) Response
+serveThumb :: AcidState StereoidDb -> AlbumId -> RouteT Sitemap App Response
 serveThumb sdb (AlbumId artid) = do
     dr <- getThumb sdb artid
     case dr of
         Just (mime,art) -> serveFileUsing filePathSendAllowRange (asContentType $ B.toString mime) $ art
         Nothing         -> serveFile (asContentType "image/png") "media_album.png"
 
-serveSong :: AcidState StereoidDb -> SongId -> RouteT Sitemap (ServerPartT IO) Response
+serveSong :: AcidState StereoidDb -> SongId -> RouteT Sitemap App Response
 serveSong sdb SongId { unSongId = songid } = do
     song <- getSongFile sdb songid
     case song of
         Nothing -> notFound $ toResponse "What you are looking for has not been found."
         Just sf -> serveFileUsing filePathSendAllowRange (asContentType "audio/mpeg3") $ C.unpack sf
 
-artistData :: AcidState StereoidDb -> ArtistId -> RouteT Sitemap (ServerPartT IO) Response
+artistData :: AcidState StereoidDb -> ArtistId -> RouteT Sitemap App Response
 artistData sdb (ArtistId id) = do
     artist <- getArtist sdb id
     case artist of
         Nothing -> notFound $ toResponse "What you are looking for has not been found."
         Just ar -> ok $ toResponse $ toJSON ar
 
-artistAlbums :: AcidState StereoidDb -> ArtistId -> RouteT Sitemap (ServerPartT IO) Response
+artistAlbums :: AcidState StereoidDb -> ArtistId -> RouteT Sitemap App Response
 artistAlbums sdb (ArtistId id) = do
     albs <- getAlbumsByArtistId sdb id
     albums <- mapM albumAddUrl albs 
     ok $ toResponse $ toJSON albums
 
-artistsAll :: AcidState StereoidDb -> RouteT Sitemap (ServerPartT IO) Response
+artistsAll :: AcidState StereoidDb -> RouteT Sitemap App Response
 artistsAll sdb = do
     artists <- getArtists sdb
     ok $ toResponse $ toJSON artists
 
-songsAll :: AcidState StereoidDb -> RouteT Sitemap (ServerPartT IO) Response
-songsAll sdb = do
+songsAll :: AcidState StereoidDb -> RouteT Sitemap App Response
+songsAll _ = do
+    sdb <- getAcidState
     ol <- getOffsetLimit
     filter <- getDataFn $ lookRead "title"
     let fSongs = case filter of
@@ -314,7 +362,7 @@ songsAll sdb = do
     songs <- mapM songAddUrl sos
     ok $ toResponse $ toJSON songs
 
-albumsAll :: AcidState StereoidDb -> RouteT Sitemap (ServerPartT IO) Response
+albumsAll :: AcidState StereoidDb -> RouteT Sitemap App Response
 albumsAll sdb = do
     ol <- getOffsetLimit
     filter <- getDataFn $ lookRead "artist"
@@ -332,7 +380,7 @@ albumsAll sdb = do
     albums <- mapM albumAddUrl albs
     ok $ toResponse $ toJSON albums
 
-getOffsetLimit :: RouteT Sitemap (ServerPartT IO) (Int,Int)
+getOffsetLimit :: RouteT Sitemap App (Int,Int)
 getOffsetLimit  = do
     r <- getDataFn $ lookRead "offset"
     s <- getDataFn $ lookRead "limit"
